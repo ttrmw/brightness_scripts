d35f828168f157c92be5931032cbc1e15e7afa54
Thomas Millward Wright <thomas@ttrmw.co.uk>
2015-02-17 23:52:29 +0000

    add bright_up script

---
+++ bright_up.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+max_brightness=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
+
+if (($brightness < $max_brightness)); then
+  let brightness=$brightness+100
+fi
+
+echo $brightness > /sys/class/backlight/intel_backlight/brightness
