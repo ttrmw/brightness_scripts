840d165b15a67d2373f4e5399706013138d4a307
Thomas Millward Wright <thomas@ttrmw.co.uk>
2015-02-18 00:04:01 +0000

    add timeout

--- bright_up.sh
+++ bright_up.sh
@@ -7,7 +7,7 @@ if (($brightness < $max_brightness)); then
   let brightness=$brightness+100
   echo $brightness > /sys/class/backlight/intel_backlight/brightness
   #notification - would be cool to do something with i3bar, though.
-  notify-send Brightness "${brightness}/${max_brightness}"
+  notify-send Brightness "${brightness}/${max_brightness}" -t 200
 fi
 
 
