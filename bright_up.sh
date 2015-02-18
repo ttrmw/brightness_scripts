e19916301ba9d8d34a08f3ed36ad7c8b20681163
Thomas Millward Wright <thomas@ttrmw.co.uk>
2015-02-18 00:00:21 +0000

    add notification, move echo

--- bright_up.sh
+++ bright_up.sh
@@ -5,6 +5,10 @@ brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
 
 if (($brightness < $max_brightness)); then
   let brightness=$brightness+100
+  echo $brightness > /sys/class/backlight/intel_backlight/brightness
+  #notification - would be cool to do something with i3bar, though.
+  notify-send Brightness "${brightness}/${max_brightness}"
 fi
 
-echo $brightness > /sys/class/backlight/intel_backlight/brightness
+
+
